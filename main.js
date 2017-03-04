1dd4f34fc6fe0aaa51bc804aeb25f5ebae4ecf66
Javiredondo <javi.redondo@live.com>
2017-03-04 19:22:01 +0100

    02 - The Different Processes

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
 
 var mainWindow = null;
 
+// listen for the "ping" event
+ipcMain.on('ping', function (event, arg) {
+  if (arg === 'hello') {
+    // send a back greeting
+    event.sender.send('pong', 'Hello, World!');
+  }
+});
+
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 800, height: 600,
+  webPreferences: {
+    nodeIntegration: false,
+    preload: __dirname + '/preload.js' // this is our preload script;
+  }});
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-})
+});
