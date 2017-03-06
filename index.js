59024190abd9792776935960b9bf15eb3e034a56
Javiredondo <javi.redondo@live.com>
2017-03-06 21:33:28 +0100

    05 - Building A Real Desktop App

--- index.js
+++ index.js
@@ -1,4 +1,89 @@
-window.ipcRenderer.on('pong', (event, arg) => {
-  document.write('<h1>' + arg + '</h1>');
+const marked = require('marked');
+const remote = require('electron').remote;
+const {dialog} = remote; // fixed incorrect usage from book
+const fs = require('fs');
+const shell = require('electron').shell;
+const ipcRenderer = require('electron').ipcRenderer;
+
+const container = document.querySelector('.container')
+const editor = document.querySelector('.editor textarea');
+const preview = document.querySelector('.preview');
+const openFileLink = document.querySelector('a.open-file');
+const saveFileLink = document.querySelector('a.save-file');
+const showFileInFolderLink = document.querySelector('a.show-file-in-folder');
+
+var currentFile = remote.getGlobal('fileToOpen') || null;
+
+if(currentFile) {
+  openFile(currentFile);
+}
+
+ipcRenderer.on('open-file', (event, filePath) => {
+  openFile(filePath);
 });
-window.ipcRenderer.send('ping', 'hello');
+
+marked.setOptions({
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+editor.onkeyup = generatePreview;
+
+openFileLink.onclick = (evt) => {
+  dialog.showOpenDialog({
+    title: 'Select a file to edit',
+    filters: [
+      { name: 'Markdown Documents', extensions: ['md', 'markdown'] }
+    ]
+  }, (filenames) => {
+    if (!filenames) return;
+    if (filenames.length > 0) {
+      openFile(filenames[0]);
+    }
+  })
+};
+
+saveFileLink.onclick = (evt) => {
+  dialog.showSaveDialog({
+    title: 'Select a file to save',
+    filters: [
+      { name: 'Markdown Documents', extensions: ['md', 'markdown'] }
+    ]
+  }, (filename) => {
+    if (!filename) return;
+    else saveFile(filename);
+  })
+};
+
+showFileInFolderLink.onclick = (evt) => {
+  shell.showItemInFolder(currentFile);
+}
+
+function generatePreview () {
+  var content = editor.value;
+  var html = marked(content);
+  preview.innerHTML = html;
+}
+
+function openFile (filename) {
+  var contents = fs.readFileSync(filename);
+
+  currentFile = filename;
+
+  editor.value = contents;
+  container.classList.remove('hidden');
+
+  generatePreview();
+
+  remote.app.addRecentDocument(filename);
+}
+
+function saveFile (filename) {
+  var content = editor.value;
+  fs.writeFileSync(filename, content);
+}
--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const ipcMain = electron.ipcMain;
 
 var mainWindow = null;
 
-// listen for the "ping" event
-ipcMain.on('ping', function (event, arg) {
-  if (arg === 'hello') {
-    // send a back greeting
-    event.sender.send('pong', 'Hello, World!');
+global.fileToOpen = null;
+
+app.on('open-file', (event, filePath) => {
+  event.preventDefault();
+  fileToOpen = filePath;
+
+  if(mainWindow) {
+    mainWindow.send('open-file', filePath);
   }
 });
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600,
-  webPreferences: {
-    nodeIntegration: false,
-    preload: __dirname + '/preload.js' // this is our preload script;
-  }});
-
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Uncomment next line to show DevTools when the app launches
+  //mainWindow.openDevTools();
 });
